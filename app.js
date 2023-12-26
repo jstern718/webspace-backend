915ac7f9c940c97137522e7ade8dfeba5d35d01d
Jonathan Stern <jstern718@gmail.com>
2023-12-26 15:47:17 -0500

    changed server to 3001, fixed db.get routes so that it is possible to retrieve all items in table, a specific item, or a specific column of a specific item

--- app.js
+++ app.js
@@ -16,71 +16,12 @@ app.get("/", (req, res, next) => {
 //TODO: Shows everything in customers, but for servers_used and applications only shows first one.
 //Not sure about other tables.
 
-app.get("/api/:table/:name/:info", (req, res, next) => {
-    let tableVar = req.params.table;
-    let infoVar = req.params.info;
-    let nameVar;
-    if (tableVar === "customers" ||
-        tableVar === "server_types" ||
-        tableVar === "resource_types" ||
-        tableVar === "code_languages" ||
-        tableVar === "software_technologies" ||
-        tableVar === "applications"){
-        nameVar = "name";
-    }
-    else{
-        nameVar = "id";
-
-    }
-    let sql = `SELECT ${infoVar} FROM ${tableVar} WHERE ${nameVar} = ?`
-    let params = [req.params.name]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
-});
-
-
-app.get("/api/:table/:name", (req, res, next) => {
-    let tableVar = req.params.table;
-    let nameVar;
-    if (tableVar === "customers" ||
-        tableVar === "server_types" ||
-        tableVar === "resource_types" ||
-        tableVar === "code_languages" ||
-        tableVar === "software_technologies" ||
-        tableVar === "applications"){
-        nameVar = "name";
-    }
-    else{
-        nameVar = "id";
-
-    }
-    let sql = `SELECT * FROM ${tableVar} WHERE ${nameVar} = ?`
-    let params = [req.params.name]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
-});
-
 app.get("/api/:table", (req, res, next) => {
     let tableVar = req.params.table;
-    let sql = `SELECT * FROM ${tableVar}`
-    let params = []
-    db.get(sql, params, (err, row) => {
+    let sql = `SELECT * FROM ${tableVar}`;
+    let params = [];
+    db.all(sql, params, (err, row) => {
+        console.log("table get");
         if (err) {
           res.status(400).json({"error":err.message});
           return;
@@ -91,6 +32,137 @@ app.get("/api/:table", (req, res, next) => {
         })
       });
 });
+
+
+app.all("/api/:table/:name", (req, res, next) => {
+    let tableVar = req.params.table;
+    let nameVar = req.params.name || null;
+    let infoVar = req.params.info || null;
+    let whereInsert;
+
+    if (tableVar === "customers" ||
+        tableVar === "server_types" ||
+        tableVar === "resource_types" ||
+        tableVar === "code_languages" ||
+        tableVar === "software_technologies" ||
+        tableVar === "applications"){
+        whereInsert = "name";
+    }
+    else{
+        whereInsert = "id";
+    }
+
+    let x = "*";
+    // if (infoVar !== null){
+    //     x = infoVar;
+    // };
+
+    // let sql;
+    let sql = `SELECT * FROM ${tableVar} WHERE ${whereInsert} = ?`;
+
+    // if (!infoVar){
+    //     let sql = `SELECT * FROM ${tableVar} WHERE ${whereInsert} = ?`;
+    // }
+    // else{
+    //     console.log("infoVar:", infoVar);
+    //     sql = `SELECT ${infoVar} FROM ${tableVar} WHERE ${whereInsert} = ${nameVar}`
+    // }
+    console.log("sql", sql);
+
+
+    let params = [nameVar]
+    db.get(sql, params, (err, row) => {
+        console.log("correct", "sql", sql, "params", params);
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
+});
+
+app.all("/api/:table/:name/:info", (req, res, next) => {
+    let tableVar = req.params.table;
+    let nameVar = req.params.name || null;
+    let infoVar = req.params.info || null;
+    let whereInsert;
+
+    if (tableVar === "customers" ||
+        tableVar === "server_types" ||
+        tableVar === "resource_types" ||
+        tableVar === "code_languages" ||
+        tableVar === "software_technologies" ||
+        tableVar === "applications"){
+        whereInsert = "name";
+    }
+    else{
+        whereInsert = "id";
+    }
+
+        let  x = infoVar;
+
+
+    // let sql;
+    let sql = `SELECT ${x} FROM ${tableVar} WHERE ${whereInsert} = ?`;
+
+    // if (!infoVar){
+    //     let sql = `SELECT * FROM ${tableVar} WHERE ${whereInsert} = ?`;
+    // }
+    // else{
+    //     console.log("infoVar:", infoVar);
+    //     sql = `SELECT ${infoVar} FROM ${tableVar} WHERE ${whereInsert} = ${nameVar}`
+    // }
+    console.log("sql", sql);
+
+
+    let params = [nameVar]
+    db.get(sql, params, (err, row) => {
+        console.log("correct", "sql", sql, "params", params);
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
+});
+
+
+// app.get("/api/:table/:name", (req, res, next) => {
+//     let tableVar = req.params.table;
+//     let nameVar;
+//     if (tableVar === "customers" ||
+//         tableVar === "server_types" ||
+//         tableVar === "resource_types" ||
+//         tableVar === "code_languages" ||
+//         tableVar === "software_technologies" ||
+//         tableVar === "applications"){
+//         nameVar = "name";
+//     }
+//     else{
+//         nameVar = "id";
+
+//     }
+//     let sql = `SELECT * FROM ${tableVar} WHERE ${nameVar} = ?`
+//     let params = [req.params.name]
+//     db.get(sql, params, (err, row) => {
+//         if (err) {
+//           res.status(400).json({"error":err.message});
+//           return;
+//         }
+//         res.json({
+//             "message":"success",
+//             "data":row
+//         })
+//       });
+// });
+
+
 
 
 // Default response for any other request
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 "Use strict;"
 const app = require("./app");
 
-app.listen(3000, function () {
-    console.log("App started at http://localhost:3000/");
+app.listen(3001, function () {
+    console.log("App started at http://localhost:3001/");
 });
 
 
